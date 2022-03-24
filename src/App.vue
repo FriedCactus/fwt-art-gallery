fc698e38ae6b3d4198efc6f517aac77723164166
FriedLemon <a.belyanchikov@framework.team@mail.ru>
2022-03-24 17:07:14 +0400

    Added preprocessor and variables

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,20 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  min-height: 100vh;
+  font-family: $RobotoRegular;
+}
+
+*,
+*::after,
+*::before {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+img {
+  max-width: 100%;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,13 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: "/fwt-art-gallery/",
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/styles/_variables.scss";
+      `,
+      },
+    },
+  },
 });
