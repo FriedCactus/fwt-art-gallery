93213c11df24aa54264dc34317548667e6ca78f5
FriedLemon <a.belyanchikov@framework.team@mail.ru>
2022-03-25 11:19:49 +0400

    Added breakpoints mixin

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ module.exports = defineConfig({
       sass: {
         additionalData: `
           @import "@/styles/_variables.scss";
+          @import "@/styles/_mixins.scss";
       `,
       },
     },
