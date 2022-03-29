cd65060c7d6c781910e42b95ab5dddfff823d39e
FriedLemon <a.belyanchikov@framework.team@mail.ru>
2022-03-29 16:25:03 +0400

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <Wrapper>
     <Header />
-    <CardsGrid />
+    <Gallery />
   </Wrapper>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Gallery from "./components/Gallery.vue";
 import Header from "./components/Header.vue";
 import Wrapper from "./components/Wrapper.vue";
-import CardsGrid from "./ui/CardsGrid.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     Wrapper,
     Header,
-    CardsGrid,
+    Gallery,
   },
 });
 </script>
